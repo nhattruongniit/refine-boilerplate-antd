03fc0e9dffe771312b9acbbfe2ee68c53819df37
Tony Nguyen <nhattruong1811@gmail.com>
2023-09-15 08:19:56 +0700

    Update NestCrudProvider.ts

--- src/libs/RestProvider.ts
+++ src/libs/RestProvider.ts
@@ -1,125 +1,28 @@
-import axios from "axios";
+import { DataProvider as DataProvider$1 } from "@refinedev/core";
+import nestjsxCrudDataProvider from "@pankod/refine-nestjsx-crud";
 
-// refine
-import {
-  AuthBindings,
-  HttpError,
-  DataProvider as DataProvider$1,
-} from "@refinedev/core";
-import nestjsxCrudDataProvider from "@refinedev/nestjsx-crud";
-import {
-  RequestQueryBuilder,
-  QueryJoin,
-  QueryJoinArr,
-} from "@nestjsx/crud-request";
+import { axiosInstance } from "./AxiosInterceptor";
 
-// interceptor
-export const axiosInstance = axios.create({});
+const apiUrlBE = "http://localhost:3000" + "/api" ;
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    let customError: HttpError = {
-      ...error,
-      message: error.response?.data?.error?.message,
-      statusCode: error.response?.status,
-    };
 
-    if (error.response?.status === 429) {
-      customError = {
-        ...customError,
-        message: error.response?.data.message,
-        statusCode: error.response?.status,
-      };
-    }
-
-    return Promise.reject(customError);
-  }
-);
-
-// Custom Data Provider
-// const apiUrlBE = import.meta.env.VITE_BACKEND_URL + "/api";
-const apiUrlBE = "http://localhost:8080/api";
-
-const handleJoin = (
-  query: RequestQueryBuilder,
-  join?: QueryJoin | QueryJoinArr | (QueryJoin | QueryJoinArr)[]
-) => {
+const handleJoin = (query: RequestQueryBuilder, join?: QueryJoin | QueryJoinArr | (QueryJoin | QueryJoinArr)[]) => {
   if (join) {
     query.setJoin(join);
   }
   return query;
 };
 
-export const CustomNestjsDataProvider = (
-  apiUrl: string,
-  httpClient: any = axiosInstance
-): DataProvider$1 => ({
+export const CustomNestCrudDataProvider = (apiUrl: string, httpClient: any = axiosInstance): DataProvider$1 => ({
   ...nestjsxCrudDataProvider(apiUrl, httpClient),
   getOne: async ({ resource, id, metaData }) => {
     const url = `${apiUrl}/${resource}/${id}`;
     let query = RequestQueryBuilder.create();
-    console.log({ metaData });
     query = handleJoin(query, metaData?.join);
-    console.log({ query: query.query(true) });
-
     const { data } = await httpClient.get(`${url}?${query.query(true)}`);
-
     return {
       data,
     };
   },
 });
-
-export const nestJsDataProvider = CustomNestjsDataProvider(
-  apiUrlBE,
-  axiosInstance
-);
-
-// auth provider
-export const authProvider: AuthBindings = {
-  login: async () => {
-    window.localStorage.setItem("accessToken", "token-test");
-    return {
-      success: true,
-    };
-  },
-  logout: async () => {
-    window.localStorage.removeItem("accessToken");
-    return {
-      success: true,
-    };
-  },
-  onError: async (error) => {
-    return { error };
-  },
-  check: async () => {
-    const accessToken = window.localStorage.getItem("accessToken");
-    if (accessToken) {
-      axiosInstance.defaults.headers.common = {
-        Authorization: "Bearer " + accessToken,
-      };
-      return {
-        authenticated: true,
-      };
-    } else {
-      return {
-        authenticated: false,
-      };
-    }
-  },
-  getPermissions: async () => null,
-  getIdentity: async () => {
-    const user = {
-      name: "truong",
-    };
-    if (user) {
-      return {
-        ...user,
-      };
-    }
-    return null;
-  },
-};
+export const nestCrudProvider = CustomNestCrudDataProvider(apiUrlBE, axiosInstance);
